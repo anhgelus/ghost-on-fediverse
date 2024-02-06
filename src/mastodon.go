98d6695d8a3e39b6908fbad71155371e8ed007df
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-02-06 15:45:46 +0100

    refactor(mastodon): put toot section in mastodon.go

--- src/mastodon.go
+++ src/mastodon.go
@@ -2,8 +2,13 @@ package src
 
 import (
 	"errors"
+	"fmt"
 	"github.com/McKael/madon"
+	"io"
+	"net/http"
 	"os"
+	"regexp"
+	"strings"
 )
 
 const (
@@ -54,4 +59,69 @@ func ConnectMastodon() error {
 		return errors.Join(err, ErrCannotConnect)
 	}
 	return nil
+}
+
+func TootPost(post CurrentPost) error {
+	var id int64
+	var err error
+	if post.FeatureImage != "" {
+		id, err = uploadMedia(post)
+		if err != nil {
+			return err
+		}
+	}
+	_, err = MastodonClient.PostStatus(genMessage(post), 0, []int64{id}, false, "", "")
+	return err
+}
+
+func uploadMedia(post CurrentPost) (int64, error) {
+	imageUrl := post.FeatureImage
+	alt := cleanText(post.FeatureImageCaption)
+
+	s := strings.Split(imageUrl, ".")
+	ext := s[len(s)-1]
+
+	res, err := http.Get(imageUrl)
+	if err != nil {
+		return 0, err
+	}
+	b, err := io.ReadAll(res.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	file := fmt.Sprintf("tmp.%s", ext)
+	err = os.WriteFile(file, b, 0666)
+	if err != nil {
+		return 0, err
+	}
+
+	att, err := MastodonClient.UploadMedia(file, alt, "")
+	if err != nil {
+		return 0, err
+	}
+
+	return att.ID, os.Remove(file)
+}
+
+func cleanText(t string) string {
+	reg1 := regexp.MustCompile("<[^/][^<]+>")
+	reg2 := regexp.MustCompile("</[^<]+>")
+	b := []byte(t)
+	v := []byte("")
+	b = reg1.ReplaceAll(b, v)
+	b = reg2.ReplaceAll(b, v)
+	return string(b)
+}
+
+func genMessage(post CurrentPost) string {
+	return fmt.Sprintf("%s\n\n%s\n%s\n\n%s", post.Title, post.CustomExcerpt, post.Url, genTags(post))
+}
+
+func genTags(post CurrentPost) string {
+	tags := ""
+	for _, t := range post.Tags {
+		tags += "#" + t.Slug
+	}
+	return tags
 }
--- src/router.go
+++ src/router.go
@@ -3,10 +3,7 @@ package src
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
-	"regexp"
 	"strings"
 	"time"
 )
@@ -77,72 +74,11 @@ func HandleWebhook(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 	}
 	post := body.Post.Current
-	var id int64
-	if post.FeatureImage != "" {
-		id, err = uploadMedia(post)
-		if err != nil {
-			LogError(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-	}
-	_, err = MastodonClient.PostStatus(genMessage(post), 0, []int64{id}, false, "", "")
+	err = TootPost(post)
 	if err != nil {
 		LogError(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusNoContent)
-}
-
-func uploadMedia(post CurrentPost) (int64, error) {
-	imageUrl := post.FeatureImage
-	alt := cleanText(post.FeatureImageCaption)
-
-	s := strings.Split(imageUrl, ".")
-	ext := s[len(s)-1]
-
-	res, err := http.Get(imageUrl)
-	if err != nil {
-		return 0, err
-	}
-	b, err := io.ReadAll(res.Body)
-	if err != nil {
-		return 0, err
-	}
-
-	file := fmt.Sprintf("tmp.%s", ext)
-	err = os.WriteFile(file, b, 0666)
-	if err != nil {
-		return 0, err
-	}
-
-	att, err := MastodonClient.UploadMedia(file, alt, "")
-	if err != nil {
-		return 0, err
-	}
-
-	return att.ID, os.Remove(file)
-}
-
-func cleanText(t string) string {
-	reg1 := regexp.MustCompile("<[^/][^<]+>")
-	reg2 := regexp.MustCompile("</[^<]+>")
-	b := []byte(t)
-	v := []byte("")
-	b = reg1.ReplaceAll(b, v)
-	b = reg2.ReplaceAll(b, v)
-	return string(b)
-}
-
-func genMessage(post CurrentPost) string {
-	return fmt.Sprintf("%s\n\n%s\n%s\n\n%s", post.Title, post.CustomExcerpt, post.Url, genTags(post))
-}
-
-func genTags(post CurrentPost) string {
-	tags := ""
-	for _, t := range post.Tags {
-		tags += "#" + t.Slug
-	}
-	return tags
 }
